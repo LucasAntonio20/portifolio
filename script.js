e53a4a2f771625604994a635c44a802735590571
Lucas Antonio <86208367+LucasAntonio20@users.noreply.github.com>
2022-12-07 12:45:11 -0300

    Implementing details in the skills images

--- script.js
+++ script.js
@@ -36,3 +36,49 @@ skillsHeader.addEventListener('click',() => {
     menu.classList.remove('allow');
     headerMenu.classList.remove('allow');
 })
+
+/* Text Skill */
+
+const html = document.getElementById('html-s');
+const htmltext = document.getElementById('html-skills-text');
+
+html.addEventListener("mouseover", function() {
+    htmltext.textContent = "HTML";
+})
+
+html.addEventListener("mouseout", function() {
+    htmltext.textContent = "";
+})
+
+const css = document.getElementById('css-s');
+const csstext = document.getElementById('css-skills-text');
+
+css.addEventListener("mouseover", function() {
+    csstext.textContent = "CSS";
+})
+
+css.addEventListener("mouseout", function() {
+    csstext.textContent = "";
+})
+
+const js = document.getElementById('js-s');
+const jstext = document.getElementById('js-skills-text');
+
+js.addEventListener("mouseover", function() {
+    jstext.textContent = "JavaScript";
+})
+
+js.addEventListener("mouseout", function() {
+    jstext.textContent = "";
+})
+
+const java = document.getElementById('java-s');
+const javatext = document.getElementById('java-skills-text');
+
+java.addEventListener("mouseover", function() {
+    javatext.textContent = "Java";
+})
+
+java.addEventListener("mouseout", function() {
+    javatext.textContent = "";
+})
